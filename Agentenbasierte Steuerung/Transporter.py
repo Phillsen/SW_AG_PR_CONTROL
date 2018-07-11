115fbd7fccd21e80abb08b3132450cfa036033eb
Philipp Diem <philipp.diem@fhnw.ch>
2018-07-11 17:03:49 +0200

    transporter suchfunktion v0.5

--- Agentenbasierte Steuerung/Transporter.py
+++ Agentenbasierte Steuerung/Transporter.py
@@ -3,7 +3,7 @@ from cozmo.util import degrees, distance_mm, speed_mmps, Pose
 from cozmo.objects import LightCube1Id, LightCube2Id, LightCube3Id
 from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 import asyncio
-
+import time
 
 distance =100 
 turndegrees = 0
@@ -12,15 +12,24 @@ speed = 100
 x_coordinate = 0
 y_coordinate = 0
 
+Suchobjekt = 1
 
 
-def ObjektGesehen(evt, **kw):
+
+
+def handle_object_appeared(evt, **kw):
     # This will be called whenever an EvtObjectAppeared is dispatched -
     # whenever an Object comes into view.
     if isinstance(evt.obj, CustomObject):
         print("Cozmo started seeing a %s" % str(evt.obj.object_type))
 
-def got_to_coordinate(robot: cozmo.robot.Robot):
+def handle_object_disappeared(evt, **kw):
+    # This will be called whenever an EvtObjectDisappeared is dispatched -
+    # whenever an Object goes out of view.
+    if isinstance(evt.obj, CustomObject):
+        print("Cozmo stopped seeing a %s" % str(evt.obj.object_type))
+
+def go_to_coordinate(robot: cozmo.robot.Robot):
     global x_coordinate
     global y_coordinate
     global angledegrees
@@ -33,29 +42,43 @@ def drive_straight(robot: cozmo.robot.Robot):
 
 def gehSuchen(robot: cozmo.robot.Robot):
 
-    cube_obj = robot.world.define_custom_cube(CustomObjectTypes.CustomType00,CustomObjectMarkers.Diamonds2, 60, 50, 50, True)
+    robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+    robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
     
-    Maschine = None
+    Maschine1 = robot.world.define_custom_cube(CustomObjectTypes.CustomType00,CustomObjectMarkers.Circles2, 50, 50, 50, True)
+    Maschine2 = robot.world.define_custom_cube(CustomObjectTypes.CustomType01,CustomObjectMarkers.Circles3, 50, 50, 50, True)
+    Maschine3 = robot.world.define_custom_cube(CustomObjectTypes.CustomType02,CustomObjectMarkers.Circles4, 50, 50, 50, True)
+    Maschine4 = robot.world.define_custom_cube(CustomObjectTypes.CustomType03,CustomObjectMarkers.Circles5, 50, 50, 50, True)
+    Maschine5 = robot.world.define_custom_cube(CustomObjectTypes.CustomType04,CustomObjectMarkers.Diamonds2, 50, 50, 50, True)
+
+    robot.set_head_angle(degrees(8)).wait_for_completed()
+    
+    # dreh dich und such nach der maschine
+
+    # positioniere dich zur maschine - 15cm Abstand
+
+    # such den würfel
+
+    # lade den Würfel auf
+
+    # fahr zutrück zur Mitte
+
+    # dreh und scanne nach der nächsten maschine
+
+    # positioniere dich zur maschine
+
+    # lad den würfel ab
+
+    
+    
+    
 
     # in die Mitte fahren
-    robot.drive_straight(distance_mm(distance), speed_mmps(speed)).wait_for_completed()
+    #robot.drive_straight(distance_mm(distance), speed_mmps(speed)).wait_for_completed()
 
     #anfangen zu suchen
-    look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-    
-    try:
-        Maschine = robot.world.wait_for_observed_light_cube(timeout=30)
-        Maschine = robot.world.wa
-               
+    for n in range(0,24):
+        robot.turn_in_place(degrees(30)).wait_for_completed()
+      
 
-    except asyncio.TimeoutError:
-        pass
-        # zurücksetzen neu suchen
-
-    finally:
-        # whether we find it or not, we want to stop the behavior
-        look_around.stop()
-
-
-
-cozmo.run_program(gehSuchen)
+cozmo.run_program(gehSuchen, use_3d_viewer=True, use_viewer=True )
